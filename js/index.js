0d290946a97ba017e1ae2d2e2520f7dddef7433e
MilimNavaZeta <saida01.02.03.04.05.06@gmail.com>
2023-10-04 20:12:32 +0600

    first commit

--- js/index.js
+++ js/index.js
@@ -1,2 +1,16 @@
+const swiper = new Swiper('.swiper', {
+
+  direction: 'vertical',
+  loop: false,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
+
 document.querySelector(".burger-lines").addEventListener("click", () => {
 document.querySelector(".burger-menu").classList.toggle("active") })
+
